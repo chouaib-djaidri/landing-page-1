35ef2e7247d9930e9496341a40ed3c8ccdf40a36
Chouaib Djaidri <chouaib.djaidri.19@gmail.com>
2020-04-13 19:50:37 +0100

    feat:implement responsive navbar

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggle = document.querySelector(".navbar__toggle");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  navbarToggle.addEventListener("click", function () {
+    mobileMenu.classList.toggle("mobile-menu--active");
+  });
+
+  // Close mobile menu when clicking on a link
+  const mobileLinks = document.querySelectorAll(".mobile-menu__link");
+  mobileLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      mobileMenu.classList.remove("mobile-menu--active");
+    });
+  });
+
+  // Close mobile menu when clicking outside
+  document.addEventListener("click", function (event) {
+    const isClickInsideNav =
+      navbarToggle.contains(event.target) || mobileMenu.contains(event.target);
+
+    if (!isClickInsideNav) {
+      mobileMenu.classList.remove("mobile-menu--active");
+    }
+  });
+});
