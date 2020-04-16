845b15873226cd8d5ef723b7d7c2fed0be4e9002
Chouaib Djaidri <chouaib.djaidri.19@gmail.com>
2020-04-17 00:55:29 +0100

    feat: add faq section

--- script.js
+++ script.js
@@ -23,4 +23,26 @@ document.addEventListener("DOMContentLoaded", function () {
       mobileMenu.classList.remove("mobile-menu--active");
     }
   });
+
+  // FAQ Accordion functionality
+  const faqQuestions = document.querySelectorAll(".faq__question");
+
+  faqQuestions.forEach((question) => {
+    question.addEventListener("click", function () {
+      const faqItem = this.parentElement;
+      const isActive = faqItem.classList.contains("faq__item--active");
+
+      // Close all FAQ items
+      document.querySelectorAll(".faq__item").forEach((item) => {
+        item.classList.remove("faq__item--active");
+        item.querySelector(".faq__question").setAttribute("aria-expanded", "false");
+      });
+
+      // If the clicked item wasn't active, open it
+      if (!isActive) {
+        faqItem.classList.add("faq__item--active");
+        this.setAttribute("aria-expanded", "true");
+      }
+    });
+  });
 });
