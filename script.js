c753121ef8037ff1c1a3c825967f3552f70e0e17
Chouaib Djaidri <chouaib.djaidri.19@gmail.com>
2020-04-24 01:52:15 +0100

    feat: add smooth scroll navigation

--- script.js
+++ script.js
@@ -45,4 +45,65 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  // Smooth scrolling and active navigation
+  const navLinks = document.querySelectorAll(".navbar__link, .mobile-menu__link");
+  const sections = document.querySelectorAll("section[id]");
+  const navbarHeight = document.querySelector(".navbar").offsetHeight;
+
+  // Smooth scrolling when clicking navigation links
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        const offsetTop = targetSection.offsetTop - navbarHeight;
+        window.scrollTo({
+          top: offsetTop,
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+
+  // Update active navigation link based on scroll position
+  function updateActiveNavLink() {
+    const scrollPosition = window.scrollY + navbarHeight + 100;
+
+    let currentSection = '';
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+        currentSection = section.getAttribute('id');
+      }
+    });
+
+    // Remove active class from all nav links
+    navLinks.forEach((link) => {
+      link.classList.remove("navbar__link--active", "mobile-menu__link--active");
+    });
+
+    // Add active class to current section's nav link
+    if (currentSection) {
+      const activeNavLink = document.querySelector(`.navbar__link[href="#${currentSection}"]`);
+      const activeMobileLink = document.querySelector(`.mobile-menu__link[href="#${currentSection}"]`);
+
+      if (activeNavLink) {
+        activeNavLink.classList.add("navbar__link--active");
+      }
+      if (activeMobileLink) {
+        activeMobileLink.classList.add("mobile-menu__link--active");
+      }
+    }
+  }
+
+  // Listen for scroll events
+  window.addEventListener("scroll", updateActiveNavLink);
+
+  // Call once on load to set initial active state
+  updateActiveNavLink();
 });
